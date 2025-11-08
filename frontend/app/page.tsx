8f660dc2a058f70afcee4ec952d7d3b9e8159d02
Aman Bani <amanbani647@gmail.com>
2025-11-08 20:19:29 +0530

    Changes in frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+// import  {HeroHeader} from "@/components/header"
+import MainSection from "@/components/hero-section"
+
+
+export default function Home(){
+  return (
+    <>
+    {/* <HeroHeader /> */}
+    <MainSection />
+    </>
+    
+  )
+}
+
