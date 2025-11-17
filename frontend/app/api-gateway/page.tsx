8e93bf0a17ce5eabccd17bd56dedaaa3a94f6d1f
Aman Bani <amanbani647@gmail.com>
2025-11-17 23:51:59 +0530

    added Load-baancer functionality

--- frontend/app/api-gateway/page.tsx
+++ frontend/app/api-gateway/page.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Image from "next/image";
+
+
+import Desktop from "@/./public/images/desktop.jpg"
+import Phone from '@/public/images/phone.jpg'
+import Laptop from '@/public/images/Laptop.jpg'
+
+import { motion } from "framer-motion";
+import Typography from '@mui/material/Typography'
+// const MotionButton = motion(Button);
 
 const page = () => {
   return (
-    <div>  Hello this us API Gateway</div>
+    <div> 
+        <div className='w-20 h-20 pt-12 ml-10'>
+        <Image src={Desktop}  alt="Desktop"  />
+        <Image src={Phone} alt="Laptop" className='mt-10' />
+        <Image src={Laptop} alt='Phone' className='mt-10' />
+        </div>
+        <div className='w-30 h-100 bg-red-500 border-2 align-middle mt-30 ml-100 '>
+    <Typography
+  variant="h3"
+  sx={{
+    color: "white",
+    fontFamily: "monospace",
+    writingMode: "sideways-lr",
+    textOrientation: "upright",
+    padding: "25px",
+    justifyContent : "center",
+    alignItems: "center",
+    alignContent: "center",
+
+  }}
+>
+  Load Balancer
+</Typography>
+        </div>
+
+
+    </div>
   )
 }
 
