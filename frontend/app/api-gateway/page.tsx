12db69742288d3fc2f66f892890c6348e983b11f
Aman Bani <amanbani647@gmail.com>
2025-12-08 22:58:53 +0530

    added API Gateway

--- frontend/app/api-gateway/page.tsx
+++ frontend/app/api-gateway/page.tsx
@@ -41,6 +41,7 @@ const page = () => {
         </div>
 
             <div className="relative flex items-center" style={{ width: '250px', height: '400px' }}>
+
           <svg width="250" height="400" className="absolute" style={{ left: 0 }}>
             <line x1="0" y1="50" x2="250" y2="200" stroke="#fbbf24" strokeWidth="3" />
             <line x1="0" y1="200" x2="250" y2="200" stroke="#fbbf24" strokeWidth="3" />
@@ -94,14 +95,49 @@ const page = () => {
         </div>
 
 
-        <div className="bg-red-500 flex items-center " style={{ width: '100px', height: '400px', marginLeft:"0px"}}>
-          <Typography
-            variant="h3"
-          sx={{ color: "white", fontFamily: "monospace", writingMode: "sideways-lr", textOrientation: "upright", padding: "25px", justifyContent : "center", alignItems: "center", alignContent: "center", }}
-          >
-            Load Balancer
-          </Typography>
-        </div>
+     <div className="flex items-center relative">
+
+  {/* Load Balancer */}
+  <div className="bg-red-500 flex items-center justify-center"
+       style={{ width: "100px", height: "400px" }}>
+    <Typography
+      variant="h3"
+      sx={{
+        color: "white",
+        fontFamily: "monospace",
+        writingMode: "sideways-lr",
+        textOrientation: "upright",
+        padding: "25px",
+      }}
+    >
+      Load Balancer
+    </Typography>
+  </div>
+
+  {/* YELLOW LINE BETWEEN LB & API GATEWAY */}
+  <svg width="300" height="400" className="absolute left-[100px]">
+    <line x1="0" y1="200" x2="300" y2="200" stroke="#fbbf24" strokeWidth="4" />
+  </svg>
+
+  {/* API Gateway */}
+  <div className="bg-red-500 flex items-center justify-center ml-[300px]"
+       style={{ width: "100px", height: "400px" }}>
+    <Typography
+      variant="h3"
+      sx={{
+        color: "white",
+        fontFamily: "monospace",
+        writingMode: "sideways-lr",
+        textOrientation: "upright",
+        padding: "25px",
+      }}
+    >
+      API Gateway
+    </Typography>
+  </div>
+
+</div>
+
 
       </div>
     </div>
